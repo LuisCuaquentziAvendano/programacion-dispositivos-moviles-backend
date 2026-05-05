9cc240038aec6e891e14ceabf0e31b3b039f3e8c
Luis Antonio Cuaquentzi Avendaño <luis.cuaquentzi@gmail.com>
2026-05-05 16:38:55 -0600

    fix: command dev must delete and create a new database

--- src/patient/dto/update-patient.dto.ts
+++ src/patient/dto/update-patient.dto.ts
@@ -2,4 +2,5 @@ import { PartialType } from '@nestjs/mapped-types';
 
 import { CreatePatientDto } from './create-patient.dto';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 export class UpdatePatientDto extends PartialType(CreatePatientDto) {}
